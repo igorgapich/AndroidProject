64b3626d54ca61ddf80f7e107b6745892e3333dc
gapichigor <guitarigor97@gmail.com>
2024-04-10 23:01:27 +0300

    Init Android project

---
+++ AndroidApp/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.example.androidapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.androidapp"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    // https://mvnrepository.com/artifact/com.github.bumptech.glide/glide
+    implementation(libs.glide)
+    annotationProcessor(libs.glideCompiler)
+}
---
+++ AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.androidapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ImageView ivAvatar = findViewById(R.id.imageView);
+        String url = "https://armyinform.com.ua/wp-content/uploads/2022/09/f_19230554251646029050.jpg";
+        Glide.with(this)
+                .load(url)
+                .apply(new RequestOptions().override(400))
+                .into(ivAvatar);
+    }
+}
