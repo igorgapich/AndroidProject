3e47565c67ef32d2ad9306054d3b58d700b790d8
gapichigor <guitarigor97@gmail.com>
2024-04-11 16:18:25 +0300

    Connection SQLite, add table Category, add controller CRUD operations

--- WebKrot/WebKrot/Program.cs
+++ WebKrot/WebKrot/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using WebKrot.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<Context>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("ContextCS")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
