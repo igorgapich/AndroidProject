009b62d62445d746b4bbe2c8cc422aee273c8cf4
Igor_Gapych <guitarigor97@gmail.com>
2024-04-15 12:03:07 +0300

    Add-Migration

--- WebKrot/WebKrot/Program.cs
+++ WebKrot/WebKrot/Program.cs
@@ -1,27 +1,34 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
-using WebKrot.Data;
+using Infrastructure;
+using Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connection = builder.Configuration.GetConnectionString("ConnectionSqlite");
+
+builder.Services.AddDBContext(connection);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<Context>(options =>
-options.UseSqlite(builder.Configuration.GetConnectionString("ContextCS")));
-
+builder.Services.AddRepository();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddValidator();
+
+builder.Services.AddAutoMapper();
+
+builder.Services.AddCustomService();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 var dir = Path.Combine(Directory.GetCurrentDirectory(), "images");
